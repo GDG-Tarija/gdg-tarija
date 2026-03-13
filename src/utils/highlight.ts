a9678d9aa7d26393cb3d39509ff9e9230677881f
Marcia Andrade <marcia.andrade.llanos@gmail.com>
2026-03-13 01:38:29 -0400

    clean components

--- src/utils/highlight.ts
+++
@@ -1,8 +0,0 @@
-import { codeToHtml } from "shiki";
-
-export async function highlightCode(code: string, lang: string = "text") {
-  return await codeToHtml(code, {
-    lang,
-    theme: "github-light",
-  });
-}
